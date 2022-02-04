825cbc1ff39d3268f070fffe9870c621e9089d8c
Scott.Moon <interactord@gmail.com>
2022-02-04 21:49:55 +0900

    feat: 이미지 회전 기능 추가

--- CameraScanExmaple/CameraScanExmaple/feature/FlatImageView.swift
+++ CameraScanExmaple/CameraScanExmaple/feature/FlatImageView.swift
@@ -12,50 +12,25 @@ struct FlatImageView: View {
   }
 
   var body: some View {
-    VStack {
-      Image(uiImage: image)
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-        .frame(minWidth: .zero, maxWidth: .infinity)
-      Button(action: {
-
-      }) {
-        Text("Ratate")
+    ZStack {
+      VStack {
+        Image(uiImage: image)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(minWidth: .zero, maxWidth: .infinity, alignment: .center)
+      }
+      VStack {
+        Spacer()
+        Button(action: {
+          guard let image = ImageDetactionFunctor().rotate(image: self.image, angle: .init(value: 90, unit: .degrees)) else { return }
+          self.image = image
+        }) {
+          Text("Ratate")
+            .frame(minWidth: .zero, maxWidth: .infinity)
+            .padding(.vertical, 30)
+            .background(Color.white)
+        }
       }
     }
-  }
-}
-
-extension UIImage {
-  /// - Note: Rotate the image by the given angle, and perform other transformations based on the passed in options.
-
-  fileprivate func rotated(angle: Measurement<UnitAngle>, options: RotationOptions = []) -> UIImage? {
-    guard let cgImage = cgImage else { return .none }
-
-    let rotationRadians = CGFloat(angle.converted(to: .radians).value)
-    let transform = CGAffineTransform(rotationAngle: rotationRadians)
-    let cgImageSize = CGSize(width: cgImage.width, height: cgImage.height)
-    var rect = CGRect(origin: .zero, size: cgImageSize).applying(transform)
-    rect.origin = .zero
-
-    let format = UIGraphicsImageRendererFormat()
-    format.scale = 1
-
-    let renderer = UIGraphicsImageRenderer(size: rect.size, format: format)
-
-    let newImage = renderer.image { renderContext in
-      renderContext.cgContext.translateBy(x: rect.midX, y: rect.midY)
-      renderContext.cgContext.rotate(by: rotationRadians)
-
-      let x = options.contains(.flipOnVerticalAxis) ? -1.0 : 1.0
-      let y = options.contains(.flipOnHorizontalAxis) ? 1.0 : -1.0
-      renderContext.cgContext.scaleBy(x: .init(x), y: .init(y))
-
-      let origin = CGPoint(x: -cgImageSize.width / 2.0, y: -cgImageSize.height / 2.0)
-      let drawRect = CGRect(origin: origin, size: cgImageSize)
-      renderContext.cgContext.draw(cgImage, in: drawRect)
-    }
-
-    return newImage
   }
 }
